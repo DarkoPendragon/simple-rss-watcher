d5849cb3ea066e87b1db31108f173c575f0e8ddf
Darko Pendragon <darkoodom@gmail.com>
2023-10-16 17:01:54 -0400

    Init commit

---
+++ README.md
@@ -0,0 +1,77 @@
+# simple-rss-watcher
+simple-rss-watcher is a rss watcher based on <a href="https://github.com/yayudev/feed-watcher#readme" target="_blank">datyayu's improved rss-watcher</a>, which fixes constant 504 returns and (slightly) optimizes the 8+ year old code.
+
+## Installation
+You can install simple-rss-watcher by using:
+```
+  npm install simple-rss-watcher --save
+```
+
+## Usage
+A basic watcher can be created using:
+```
+  var Watcher  = require('simple-rss-watcher'),
+  var rssUrl   = 'http://lorem-rss.herokuapp.com/feed?unit=second&interval=5'
+
+  // if no interval is passed, 60s will be set as the default interval
+  // but you MUST pass 'feedUrl' or you'll get an error
+  var watcher = new Watcher({ feedUrl: rssUrl, interval: 10 })
+
+  // This will emit if the script finds new itmes at your interval time
+  watcher.on('new entries', (entries) => {
+    entries.forEach((entry) => {
+      console.log(entry.title)
+    })
+  })
+
+  // Start watching the feed.
+  watcher.start().then((entries) => {
+    // do something
+    console.log(entries)
+  })
+  .catch((error) => {
+    console.error(error)
+  })
+
+  // Stop watching the feed.
+  watcher.stop()
+
+  // This will emit a "stop" event
+  watcher.on('stop', () => {
+    console.log('Watcher stopped!')
+  })
+```
+
+### Options
+If you want to change the watcher config after creating it, you can use watcher.config():
+```
+  watcher.config({ feedUrl: feed, interval: 60 })
+```
+
+### Events
+Watcher exposes 3 events: 'new entries', 'stop' and 'error'.
+```
+  // Returns an array of entry objects founded since last check
+  watcher.on('new entries', (entries) => {
+    console.log(entries)
+  })
+
+  // Emitted when watcher.stop() is called
+  watcher.on('stop', () => {
+    console.log('stopped')
+  })
+
+  // Emitted when an error happens while checking feed
+  watcher.on('error', (error) => {
+    console.error(error)
+  })
+```
+
+## Changes from datyayu's script
+I really didn't spend much time on this, but here's all the small changes I made:
+* Switched from `parse-rss` to `rss-parser` (fixes 504 returns)
+* Removed some useless fucntions
+* The Watcher constructer takes an Object instead of 2 values
+* Will error out if a non-number is passed for an interval
+* The request (entries fetching) function is now nested in the constructor
+* Some minor (non-breaking) typos were fixed
---
+++ src/index.js
@@ -0,0 +1,96 @@
+import {EventEmitter} from 'events';
+import Parser from 'rss-parser';
+const prs = new Parser();
+
+class Watcher extends EventEmitter {
+  constructor(options) {
+    super();
+
+    if (!options.feedUrl || typeof options.feedUrl !== 'string') {
+      throw new Error('feedUrl is not defined!');
+    }
+
+    if (options?.interval && !parseInt(options?.interval)) {
+      throw new Error('interval must be a number!');
+    }
+
+    this.feedUrl = options.feedUrl;
+    this.interval = options?.interval || 60;  // 1 minute by default.
+    this.lastEntryDate = null; // Used to make sure if there is new entries.
+    this.lastEntryTitle = null; // Used to avoid duplicates.
+    this.timer = null; // Stores watcher function.
+  }
+
+  request(feedUrl = this.feedUrl) {
+    return new Promise(async (resolve, reject) => {
+      var feed = await prs.parseURL(feedUrl);
+      if (feed.items.length == 0) return reject(new Error('No entries were found, check your url'));
+      feed.items.sort((ent1, ent2) => ((new Date(ent2.pubDate) / 1000) - (new Date(ent1.pubDate) / 1000)));
+      resolve(feed.items);
+    });
+  }
+
+  // Check all entries on the feed.
+  // checkAll() {
+  //   return request(this.feedUrl);
+  // }
+  // No idea why this was used instead of nesting 'request' and calling that
+
+  // Set up the watcher.
+  config(cfg) {
+    this.feedUrl = cfg?.feedUrl || this.feedUrl;
+    this.interval = cfg?.interval || this.interval;
+  }
+
+  // Start watching.
+  start() {
+    return new Promise((resolve, reject) => {
+      this.request()
+        .then((entries) => {
+          this.lastEntryDate = new Date(entries[0].pubDate) / 1000;
+          this.lastEntryTitle = entries[0].title;
+          this.timer = this.watch();
+          resolve(entries);
+        })
+        .catch((err) => {
+          reject(err);
+        });
+    });
+  }
+
+  // Stop watching.
+  stop() {
+    clearInterval(this.timer);
+    this.emit('stop');
+  }
+
+  // Check the feed.
+  watch() {
+    const fetch = () => {
+      this.request()
+        .then((entries) => {
+          // Filter older entries.
+          const newEntries = entries.filter((entry) => {
+            return (this.lastEntryDate === null || this.lastEntryDate < (new Date(entry.pubDate) / 1000));
+          });
+
+          // Update last entry.
+          // It uses newEntries[0] because they are ordered from newer to older.
+          if (newEntries.length > 0) {
+            this.lastEntryDate = new Date(newEntries[0].pubDate) / 1000;
+            this.lastEntryTitle = newEntries[0].title;
+            this.emit('new entries', newEntries);
+          }
+        })
+        .catch(error => this.emit('error', error));
+    };
+
+    // Keep checking every n minutes.
+    // It returns the timer so it can be cleared after.
+    return setInterval(() => {
+      fetch();
+    }, this.interval * 1000);
+  }
+}
+
+export default Watcher;
